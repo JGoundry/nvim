ba496a951d41160e0e4742198cc149736fca1f0d
Josh Goundry <joshuagoundry@gmail.com>
2025-12-21 11:23:48 +0000

    Add keybinds for cnext and cprev.

--- lua/joshg/remap.lua
+++ lua/joshg/remap.lua
@@ -1,3 +1,7 @@
 -- Leader key and :Ex map
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Quickfix move up and down
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
